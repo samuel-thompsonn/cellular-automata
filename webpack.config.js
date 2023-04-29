4a8841ab34c97db855527ae6fdf7169ef5c9eea9
Samuel Thompson <sam.thompson356@gmail.com>
2023-04-28 23:50:05 -0400

    change title

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Cellular Automata',
     }),
   ],
   devtool: 'inline-source-map',
